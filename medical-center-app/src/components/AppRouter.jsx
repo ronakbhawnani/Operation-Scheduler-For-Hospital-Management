62b749b66e8f350aa465d7ea9a6a452d4bb0dba3
Ronak Bhawnani <101629570+ronakbhawnani@users.noreply.github.com>
2024-08-10 01:00:43 +0530

    Add files via upload

---
+++ medical-center-app/src/components/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import FAQ from '../pages/FAQ';
+import Services from '../pages/Services';
+import Categories from '../pages/Categories';
+import NotFound from '../pages/NotFound';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Logout from './Logout';
+import Orders from '../pages/Orders';
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/orders" element={<Orders />} />
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+};
+
+export default AppRouter;
